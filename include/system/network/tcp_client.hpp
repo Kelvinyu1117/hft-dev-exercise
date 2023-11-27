2842d9cbb0eb3645861740d6f2ffae0a35951537
Kelvin Wu <kelvinyu1117@gmail.com>
2023-11-27 23:59:37 +0800

    add socket

--- include/system/network/tcp_client.hpp
+++ include/system/network/tcp_client.hpp
@@ -3,7 +3,13 @@
 #include "system/event/i_event_handler.hpp"
 #include "system/io/epoll.hpp"
 #include "system/io/reactor.hpp"
+#include <arpa/inet.h>
+#include <cstdint>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <string_view>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 namespace network {
 
@@ -13,15 +19,25 @@ template<typename T, typename Traits> class TCPClient : public event::IEventHand
   constexpr static auto RxBufferSize = Traits::RxBufferSize;
 
 public:
-  explicit TCPClient() {}
+  explicit TCPClient(std::string_view endpoint, uint16_t port) : endpoint_(endpoint), port_(port) {}
 
-  template<typename Reactor> void init(Reactor &reactor) { reactor.add_event_handler(this, EPOLLIN | EPOLLERR); }
+  void start() noexcept
+  {
+    fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+    {
+      sockaddr_in dest;
+      memset(&dest, 0, sizeof(sockaddr_in));
+      dest.sin_family = AF_INET;
+      dest->sin_addr.s_addr = inet_addr(endpoint_.c_str());
+      dest.sin_port = htons(port_);
 
-  void connect(std::string_view endpoint, uint16_t port) noexcept {}
+      if (connect(fd_, (sockaddr *)&dest, sizeof(dest)) < 0) {}
+    }
+  }
 
   void send(const std::byte *const data, size_t cnt) noexcept {};
 
-  int fd() noexcept { return -1; }
+  int fd() noexcept { return fd_; }
 
   void on_event(uint32_t event_mask) noexcept
   {
@@ -31,6 +47,8 @@ public:
 
 private:
   std::byte rx_buffer_[RxBufferSize];
+  std::string endpoint_;
+  uint16_t port_;
   int fd_;
 };
 }// namespace network
